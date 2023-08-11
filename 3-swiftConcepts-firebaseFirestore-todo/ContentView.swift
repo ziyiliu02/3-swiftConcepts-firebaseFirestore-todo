bdb24f9a929e4732b67e862579b25ef849aced02
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-11 13:55:22 +0800

    Initial Commit

---
+++ 3-swiftConcepts-firebaseFirestore-todo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  3-swiftConcepts-firebaseFirestore-todo
+//
+//  Created by Liu Ziyi on 11/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 3-swiftConcepts-firebaseFirestore-todo/__swiftConcepts_firebaseFirestore_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swiftConcepts_firebaseFirestore_todoApp.swift
+//  3-swiftConcepts-firebaseFirestore-todo
+//
+//  Created by Liu Ziyi on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct __swiftConcepts_firebaseFirestore_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
